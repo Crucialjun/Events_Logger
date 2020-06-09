04179133c79b74d945b19b99f2873d68eee5f939
Crucial Jun <nich.otieno@gmail.com>
2020-06-09 16:06:15 +0300

    Open New Activity on get stated press

--- app/src/main/java/com/example/eventslogger/OnboardingActivity.kt
+++ app/src/main/java/com/example/eventslogger/OnboardingActivity.kt
@@ -1,11 +1,20 @@
 package com.example.eventslogger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_onboarding.*
 
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+
+        button_getstarted.setOnClickListener {
+            val intent : Intent = Intent(this,MainActivity::class.java);
+            startActivity(intent)
+
+        }
     }
 }
